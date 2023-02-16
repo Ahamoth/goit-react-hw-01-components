f0cebc90292da36e5a15492afe39bd7966da9991
Ahamoth <beeminer.store@gmail.com>
2023-02-16 17:43:47 +0200

    task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import user from './../user.json';
 import data from './../data.json';
+import friends from './../friends.json';
 import { Profile } from 'components/profile/Profile';
 import { Statistics } from 'components/statistics/Statistics';
-
+import { FriendList } from 'components/friendList/FriendList';
 
 
 export const App = () => {
@@ -15,7 +16,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-     <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 };
