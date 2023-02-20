14f71b444edcd30900488ebc68d56eeaaaab9ad8
Ahamoth <beeminer.store@gmail.com>
2023-02-20 10:37:45 +0200

    task 4

---
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import css from "./TransactionHistory.module.css";
+import PropTypes from "prop-types";
+
+export const TransactionHistory = ({items}) => (
+  <table className={css.table}>
+  <thead>
+    <tr>
+      <th className={css.thead}>Type</th>
+      <th className={css.thead}>Amount</th>
+      <th className={css.thead}>Currency</th>
+    </tr>
+  </thead>
+  <tbody>
+    {items.map(item => 
+      <tr key={item.id}>
+        <td className={css.capitilize}>{item.type}</td>
+        <td>{item.amount}</td>
+        <td>{item.currency}</td>
+      </tr>
+    )}
+  </tbody>
+</table>
+)
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object)
+}
